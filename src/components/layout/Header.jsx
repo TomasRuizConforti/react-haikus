b1bc7ff3615b77e6cc92f4d267aeeed9ec69c8f0
AgusLZ <90332451+AlejandraMancilla@users.noreply.github.com>
2021-10-18 13:23:53 -0300

    Primera Parte

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import "./Header.css";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
   return (
     <header>
       <div id="cajaBuscador">
         <input type="text" name="buscar" id="buscar" placeholder="Buscar" />
-        <img
-          id="lupa"
-          src="./iconoBuscar.svg"
-          alt="/"
-          width="32px"
-          height="32px"
-        />
+        <FontAwesomeIcon icon={faSearch} id="lupa" width="32px" height="32px" />
       </div>
     </header>
   );
---
+++ src/components/layout/LinkHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./LinkHeader.css"
+
+const LinkHeader = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LinkHeader
